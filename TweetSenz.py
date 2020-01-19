8cd9fd5dab7d5501e9f0da5be2d9419b0ec86573
Sophie Berger <sophie.berger@tailored-apps.com>
2020-01-18 19:02:16 -0800

    gitignore added

--- TweetSenz.py
+++ TweetSenz.py
@@ -1,18 +1,13 @@
 import tweepy
 from textblob import TextBlob
+import config
 
-consumer_key = ''
-consumer_secret = ''
-
-access_token = ''
-access_token_secret = ''
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
-tweets = api.search('Trump')
+tweets = api.search('Austria')
 
 for tweet in tweets :
     analysis = TextBlob(tweet.text)
---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key = 'NXwbRE4Bc5RXRZuXN1gtNb2GZ'
+consumer_secret = '759AhtUJUaDtH99ps4EzFo03VCPDtX5NvxbnopHmGrbv1I6sPj'
+
+access_token = '2734831937-fFDrSP30H8dqu3Dn0LWT3v9erbSjEPnXewR1lUl'
+access_token_secret = 'RXiPod0Mceqyy7K7GYsZYtkPk2Rzu5S68CXd1dgIznYpu'
